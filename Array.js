0384dc9e6f144a638b90af2e51c9aa4f97be7552
Kanik Tyagi <kaniktyagi111@gmail.com>
2026-08-20 15:14:51 +0530

    Initial commit

---
+++ Array.js
@@ -0,0 +1,43 @@
+let c = [1,2,3,4,5];
+
+// push
+// c.push(6);
+// console.log(c);
+
+
+// pop
+// c.pop();
+// console.log(c);
+
+// length of array
+// console.log(c.length);
+
+// unshift -> adds a value in front of the array
+// c.unshift(0);
+// console.log(c);
+
+// shift -> removes the first value of the array
+// c.shift();
+// console.log(c);
+
+
+// slice -> returns a portion of the array
+// let d = c.slice(1,3);
+// console.log(d);
+
+
+// splice -> removes a portion of the array and adds new values(index, how many to remove, new values)
+c.splice(1,2,7,8);
+console.log(c);
+
+
+// reverse -> reverses the array
+// c.reverse();
+// console.log(c);
+
+
+
+
+
+
+
---
+++ String.js
@@ -0,0 +1,8 @@
+let b = new String("Hello World");
+
+// indexOf -> returns the index of the first occurrence of a specified value in a string
+console.log(b.indexOf("e")); 
+
+// lastIndexOf -> returns the index of the last occurrence of a specified value in a string
+console.log(b.lastIndexOf("e"));
+
